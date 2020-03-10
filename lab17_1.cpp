130782867715d6254b3d39cc0e6320fc23d512ff
BankChana <59277872+BankChana@users.noreply.github.com>
2020-03-10 17:16:21 +0700

    Update lab17_1.cpp
    V.3

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -1,90 +1,76 @@
-//Write your code here
 #include <iostream>
-#include <cstring> 
 #include <string>
 #include <fstream>
-#include <cctype>
 #include <vector>
-using namespace std;
+using namespace std ;
+
+string toupper_for_str(string x){
+    string y = x ;
+    for(int i = 0;i < x.size();i++){
+        y[i] = toupper(x[i]);
+    }
+    return y ;
+}
 
 int main(){
-
-    ifstream fin("name_score.txt");
-    string s,rule,fix,fix2;
-    vector<string> a;
-    vector<string> grade;
-    char name[100],point[100];
-    int na,nb,nc,nd = 0;
-    int count=0,check =0,check2 =0;
-    while(getline(fin,s)){
-        a.push_back(s);
-        count++;
-
-    }
-    for(int i=0;i<count;i++){
-        const char *c = a[i].c_str();
-        char format[] = "%[^:]: %d %d %d";
-        sscanf(c,format,name,&na,&nb,&nc);
-        nd = na+nb+nc;
-        a.at(i)=name;
-        grade.push_back(nd >= 80 ? "A": nd >= 70 ? "B" : nd >= 60 ? "C" : nd >= 50 ? "D" :"F");
-
-    }
-    string command;
-
-    for(;;){
-
-        cout << "---------------------------------\n";
-        cout << "Please input your command: ";
-        getline(cin,command);
-         if(rule == "exit")
-            break;
-        cout << "---------------------------------\n";
-        rule = command.substr(0,5);
-        if(rule == "name "|| "NAME" ){
-            command = command.erase(0,5);
-            for(int i=0;i<count;i++){
-                fix = a[i];
-                for(int j=0;j<fix.size();j++){
-                    fix[j]=toupper(fix[j]); 
-                }
-                for(int k=0;k<command.size();k++){
-                    command[k]=toupper(command[k]); 
-                }
-                if(command == fix){
-                    cout << a[i] << "'s grade = " << grade[i] << "\n" ;
-                    check2++;
-                }
-            }
-            if(check2 ==0){
-                cout <<"Cannot found.\n";
-            }
-
-
-        }else if(rule == "grade"){
-            command = command.erase(0,6);
-            
-            for(int i=0;i<count;i++){
-                for(int k=0;k<command.size();k++){
-                    command[k]=toupper(command[k]); 
-                }
-
-                if(command == grade[i]){
-                    cout << a[i] << "\n";
-                    check++;
-                }
-            }
-            if(check ==0){
-                cout <<"Cannot found.\n";
-            }
-
-        }else if(rule == "exit"){
-            break;
-        }else{
-            cout << "Invalid command\n";
-        }
+    string asd ;
+    string qwe ;
+    string textline ;
+    ifstream source ;
+    vector <string> nam ;
+    vector <float> score ;
+    vector <string> grade ;
+    string grade_1 ;
+    source.open("name_score.txt") ;
+    while(getline(source,textline)){
+        char format[] = "%[^:]: %f %f %f" ;
+        char name[100] ;
+        float a , b , c ;
+        sscanf(textline.c_str(),format,name,&a,&b,&c) ;
+        /*cout << name << endl ;
+        cout << a << " " << b << " " << c << endl ;*/
+        if(a+b+c >= 80) grade_1 = "A" ;
+            else if(a+b+c >= 70) grade_1 = "B" ;
+                else if(a+b+c >= 60) grade_1 = "C" ;
+                    else if(a+b+c >=50) grade_1 ="D" ;
+                        else grade_1 = "F" ;
+        nam.push_back(name) ;
+        score.push_back(a+b+c) ;
+        grade.push_back(grade_1) ;
 
     }
 
+    for(int i=0; ; i++){
+        cout << "Please input your command :" ;
+        getline(cin,asd) ;
+        asd = toupper_for_str(asd);
+        if(asd == "EXIT") break ;
+            else if(asd.substr(0,6) == "GRADE " ){
+                for(int j=0;j<26;j++){
+                    if(asd.substr(6,1) == grade[j] ){
+                        cout << nam[j] << endl ;
+                    }
+                    if(asd.substr(6,1) != "A" & asd.substr(6,1) != "B" & asd.substr(6,1) != "C" & asd.substr(6,1) != "D" & asd.substr(6,1) != "F" ){
+                       cout << "Cannot Found." << endl ;
+                      break ;
+                    }
+                   
+                }
+            }
+            else if(asd.substr(0,5) == "NAME "){
+                for(int k=0;k<26;k++){
+                    qwe = toupper_for_str(nam[k]);
+                    if(asd.substr(5,asd.size()-5) == qwe){
+                        cout << nam[k] <<"'s "<< "grade = " << grade[k] << endl;
+                        break ;
+                    }
+                    if(k == 25 ) cout << "Cannot found." << endl ;
+                }
+            }
+                else cout << "Invalid command" << endl;
+        cout << "---------------------------------------------" << endl ;
+    }
 
+
+    return 0 ;
 }
